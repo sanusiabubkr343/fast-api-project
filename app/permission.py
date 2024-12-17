7ddf0721fe9fb4cc9f6a114cf0093cf990c215bc
sanusi abubakr <sanusiabubakr343@gmail.com>
2024-12-17 13:40:29 +0100

    added permission to protect endpoint

--- app/permission.py
+++ app/permission.py
@@ -6,5 +6,13 @@ def is_admin(token: str) -> bool:
     decoded = decode_access_token(token)
     role = decoded["role"]
     if role != "admin":
-        raise HTTPException(status_code=403, detail="only Admin can access this endoint")
+        raise HTTPException(status_code=403, detail="only Admin can access this endpoint")
+    return True
+
+
+def is_regular_user(token: str) -> bool:
+    decoded = decode_access_token(token)
+    role = decoded["role"]
+    if role != "regular":
+        raise HTTPException(status_code=403, detail="only Regulsr User can access this endpoint")
     return True
